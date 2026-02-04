75431a4dee1101f6e515529ee4410ccaf76c35f8
WhyFenceCode <whyfencecode@gmail.com>
2026-02-04 19:38:24 +0000

    fix: main.py: Adjust MotorDrive calls to scale deviation by 100

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def DriverControl():
         currentDistance = GetDistance() + DISTANCEHOLDOFFSET
         targetDistance = MIN + DISTANCEHOLDOFFSET
         deviation = PercentDeviation(currentDistance, targetDistance)
-        MotorDrive(deviation)
+        MotorDrive(deviation * 100)
     MotorDrive(0)
  
 def AutonomousControl():
@@ -78,7 +78,7 @@ def AutonomousControl():
         currentDistance = GetDistance()
         targetSpeed = speed_target(currentDistance)
         deviation = PercentDeviation(currentSpeed, targetSpeed)
-        MotorDrive(deviation)
+        MotorDrive(deviation * 100)
     MotorDrive(0)
 
 DriverControl()
