4d8bd987ec0e772b611efb99396a9f4abd2fa564
WhyFenceCode <whyfencecode@gmail.com>
2026-02-05 17:54:02 -0800

    add: new motor control functions

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #REPLACE THESE WITH THE ACTUAL FUNCTION WHERE THEY OCCUR
+from time import time
+
+
 centerButtonPressed = False
 topButtonPressed = False
 bottomButtonPressed = False
@@ -10,19 +13,36 @@ MAX = 0
 MIN = 0
 DISTANCEHOLDOFFSET = 10 #offset to reduce the level of %dev while holding position. increase to lower responsiveness
 
+leftmotor = None
+rightmotor = None
+distancesensor = None
+
+def Setup():
+    leftmotor = Motor("A", positive_direction = Direction.CLOCKWISE, gears = None)
+    rightmotor = Motor("B", positive_direction = Direction.COUNTERCLOCKWISE, gears = None)
+    distancesensor = UltrasonicSensor("1")
+
 def clamp(value, minimum, maximum):
     return max(minimum, min(value, maximum))
 
 
 def PercentDeviation(current, expected):
-    return clamp((expected - current) / expected, -1, 1)
+    try:
+        currentDeviation =  clamp((expected - current) / expected, -1, 1)
+    except:
+        currentDeviation = 0
+    return currentDeviation
 
 def MotorDrive(speed):
-    clockwiseMotor = speed
-    counterClockwiseMotor = -speed
+    leftmotor.dc(speed)
+    rightmotor.dc(speed)  
+
+def MotorHold():
+    leftmotor.hold()
+    rightmotor.hold()  
 
 def GetDistance():
-    return 0.5
+    return distancesensor.distance(silent=False)
 
 def GetSpeed():
     return -0.01
@@ -56,36 +76,42 @@ def speed_target(distance):
 def DriverControl():
     while not centerButtonPressed:
         if topButtonPressed:
-            MotorDrive(25)
+            MotorHold(25)
         elif bottomButtonPressed:
-            MotorDrive(-20)
+            MotorHold(-20)
         else:
-            MotorDrive(0)
+            MotorHold(0)
     MAX = GetDistance()
+    while centerButtonPressed:
+        MAX = MAX
     while not centerButtonPressed:
         if topButtonPressed:
-            MotorDrive(25)
+            MotorHold(25)
         elif bottomButtonPressed:
-            MotorDrive(-20)
+            MotorHold(-20)
         else:
-            MotorDrive(0)
+            MotorHold(0)
     MIN = GetDistance()
+    while centerButtonPressed:
+        MIN = MIN
     while not centerButtonPressed:
-        currentDistance = GetDistance() + DISTANCEHOLDOFFSET
-        targetDistance = MIN + DISTANCEHOLDOFFSET
-        deviation = PercentDeviation(currentDistance, targetDistance)
-        MotorDrive(deviation * 100)
-    MotorDrive(0)
+        MotorHold()
+        
  
 def AutonomousControl():
     TIME = TIME_UP
+    lastDistance = MIN
+    lastTime = time.time_ns()
     while GetDistance() < MAX:
-        currentSpeed = GetSpeed()
+        currentTime = time.time_ns()
         currentDistance = GetDistance()
+        currentSpeed = lastDistance - currentDistance / ((currentTime - lastTime) / 1e9)
+        lastDistance = currentDistance
+        lastTime = currentTime
         targetSpeed = speed_target(currentDistance)
         deviation = PercentDeviation(currentSpeed, targetSpeed)
         MotorDrive(deviation * 100)
-    MotorDrive(0)
+    MotorHold()
 
 DriverControl()
 
